3acdd8fc20c5f6e08ff3c86056aed77a76c5ed96
hayley2300 <hayley2300@gmail.com>
2021-06-25 07:54:32 +0900

    usermenu

--- main/java/com/basin/adapter/usermenu_adapter.java
+++ main/java/com/basin/adapter/usermenu_adapter.java
@@ -8,7 +8,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,13 +33,13 @@ public class usermenu_adapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     private ArrayList<UserMenu> mData = null;
     Util util = null;
     int posi;
-    int cate, icon;
-    TextView nav_menu_setting;
-    ImageView main_card_icon;
+    TextView menu_recycle_name;
+    EditText menu_recycle_name2;
+    ImageView menu_recycle_mdoify, menu_recycle_delete,  menu_recycle_mdoify2, menu_recycle_delete2 ;
+    LinearLayout menu_recycle_layout, menu_recycle_layout2;
     static HashMap<String, Integer> position_manager = new HashMap<String, Integer>();
     private static final int VIEW_TYPE_TEXT = 0;
     private static final int VIEW_TYPE_IMAGE = 1;
-    main_content_card item;
     RecyclerView.ViewHolder holder_;
 
     // 생성자에서 데이터 리스트 객체를 전달받음.
@@ -59,12 +61,12 @@ public class usermenu_adapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (viewType == VIEW_TYPE_TEXT) {
             View itemView = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.main_recycle_card, parent, false);
+                    .inflate(R.layout.menu_recycle_card, parent, false);
 
             return new ViewHolder(itemView);//ViewHolder
         } else {
             View itemView = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.main_recycle_card, parent, false);
+                    .inflate(R.layout.menu_recycle_card, parent, false);
 
             return new ViewHolder(itemView);// ImageViewHolder
         }
@@ -79,54 +81,19 @@ public class usermenu_adapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         }
         posi = position;
 
-        title = ((ViewHolder) holder).title;
-        title.setText(mData.get(holder_.getAdapterPosition()).getTitle());
-        Log.d("position111111", position + "");
 
+        String getname1=mData.get(holder_.getAdapterPosition()).getUser_menu1();
+        menu_recycle_name = ((ViewHolder) holder).menu_recycle_name;
+        menu_recycle_name2 = ((ViewHolder) holder).menu_recycle_name2;
 
-        content = ((ViewHolder) holder).content;
-        content.setText(mData.get(holder_.getAdapterPosition()).getContent());
-
-        tag1 = ((ViewHolder) holder).tag1;
-        String gettag1=mData.get(holder_.getAdapterPosition()).getTag1();
-        String gettag2=mData.get(holder_.getAdapterPosition()).getTag2();
-        String gettag3=mData.get(holder_.getAdapterPosition()).getTag3();
-        if(gettag1!=null && !gettag1.trim().equals(""))
-            tag1.setText("#"+mData.get(holder_.getAdapterPosition()).getTag1());
-        else
-            tag1.setText("");
-
-        tag2 = ((ViewHolder) holder).tag2;
-
-        if(gettag2!=null && !gettag2.trim().equals(""))
-            tag2.setText("#"+mData.get(holder_.getAdapterPosition()).getTag2());
-        else
-            tag2.setText("");
-
-        tag3 = ((ViewHolder) holder).tag3;
-        if(gettag3!=null && !gettag3.trim().equals(""))
-            tag3.setText("#"+mData.get(holder_.getAdapterPosition()).getTag3());
-        else
-            tag3.setText("");
-
-        url = ((ViewHolder) holder).url;
-        url.setText(mData.get(holder_.getAdapterPosition()).getUrl());
-
-        num_phase = ((ViewHolder) holder).num_phase;
-        num_phase.setText(mData.get(holder_.getAdapterPosition()).getNum_phase());
-
-        main_card_cate  = ((ViewHolder) holder).main_card_cate;
-        if(mData.get(holder_.getAdapterPosition()).getCate()>-1)
-            main_card_cate.setText(mData.get(holder_.getAdapterPosition()).getCate()+"");
-        else
-            main_card_cate.setText(0+"");
-
-        main_card_icon = ((ViewHolder) holder).main_card_icon;
-        if(mData.get(holder_.getAdapterPosition()).getIcon()>0){
-            main_card_icon.setImageDrawable(GetDrawbleIcon.getIcon(mData.get(holder_.getAdapterPosition()).getIcon(),mContext));
+        if(getname1!=null && !getname1.trim().equals("")){
+            menu_recycle_name.setText(mData.get(holder_.getAdapterPosition()).getUser_menu1());
+           // menu_recycle_name2.setText(mData.get(holder_.getAdapterPosition()).getUser_menu1());
         }else{
-            main_card_icon.setImageDrawable(null);
+            menu_recycle_name.setText("");
+          //  menu_recycle_name2.setText("");
         }
+
 
     }
 
@@ -150,81 +117,42 @@ static class ImageViewHolder extends RecyclerView.ViewHolder {
 }*/
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        TextView userid, user_menu1;
+        TextView  menu_recycle_name;
+        EditText menu_recycle_name2;
+        ImageView menu_recycle_mdoify, menu_recycle_delete,  menu_recycle_mdoify2, menu_recycle_delete2 ;
+        LinearLayout menu_recycle_layout, menu_recycle_layout2;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             //image = itemView.findViewById(R.id.imageView);
-            title = itemView.findViewById(R.id.title);
-            content = itemView.findViewById(R.id.content);
-            this.itemView = itemView;
-            num_phase = itemView.findViewById(R.id.num_phase);
-            title.setTag(getAdapterPosition());
-            Log.d("position : ", getAdapterPosition() + "!");
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int pos = getAdapterPosition();
-                    if (pos != RecyclerView.NO_POSITION) {
-                        // 데이터 리스트로부터 아이템 데이터 참조.
-                        //RecyclerItem item = mData.get(pos) ;
-                        Log.d("position ~~: ", getAdapterPosition() + "!!!");
-                        //  mData.get(pos)
-                        // TODO : use item.
-                        main_content_card mcc = mData.get(pos);
-                        Intent intent = new Intent(Intent.ACTION_VIEW);
-                        String url = mcc.getUrl().trim();
-                        if (!url.startsWith("http://") && !url.startsWith("https://"))
-                            url = "http://" + url;
-                        intent.setData(Uri.parse(url));
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        mContext.startActivity(intent);
-                        Toast.makeText(mContext, "일반클릭", Toast.LENGTH_SHORT).show();
-                        Log.d("position 1!!!: ", holder_.getAdapterPosition() + "!");
-                    }
-                }
-            });
-            itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    int pos = getAdapterPosition();
-                    if (pos != RecyclerView.NO_POSITION) {
-                        main_content_card mcc = mData.get(pos);
-                        LayoutInflater inflater = LayoutInflater.from(mContext);
-                        Intent intent = new Intent(mContext, content_modi_dialog.class);
-                        intent.putExtra("data", "Test Popup");
-                        intent.putExtra("title", mcc.getTitle());
-                        intent.putExtra("description", mcc.getContent());
-                        intent.putExtra("url", mcc.getUrl());
-                        intent.putExtra("tag1", mcc.getTag1());
-                        intent.putExtra("tag2", mcc.getTag2());
-                        intent.putExtra("tag3", mcc.getTag3());
-                        intent.putExtra("phase_num", mcc.getNum_phase());
-                        intent.putExtra("position",pos);
-                        intent.putExtra("cate",mcc.getCate());
-                        intent.putExtra("icon",mcc.getIcon());
-                        ((Activity) mContext).startActivityForResult(intent, 1);
-                        //mContext.startActivity(intent);
-                        Toast.makeText(mContext, "롱클릭", Toast.LENGTH_SHORT).show();
-                        Log.d("position2!!! : ", getAdapterPosition() + "tag :"+mcc.getTag1()+","+mcc.getTag2()+","+mcc.getTag3()+" icon:"+mcc.getIcon());
-                    }
-                    return true;  //true 설정
+            menu_recycle_name = itemView.findViewById(R.id.menu_recycle_name);
+            menu_recycle_name2 = itemView.findViewById(R.id.menu_recycle_name2);
+            menu_recycle_layout = itemView.findViewById(R.id.menu_recycle_layout1);
+            menu_recycle_layout2 = itemView.findViewById(R.id.menu_recycle_layout2);
+            menu_recycle_mdoify  = itemView.findViewById(R.id.menu_recycle_mdoify);
+            menu_recycle_delete  = itemView.findViewById(R.id.menu_recycle_delete);
+            menu_recycle_mdoify2  = itemView.findViewById(R.id.menu_recycle_mdoify2);
+            menu_recycle_delete2  = itemView.findViewById(R.id.menu_recycle_delete2);
+            menu_recycle_mdoify.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int pos = getAdapterPosition();
+                if (pos != RecyclerView.NO_POSITION) {
 
+                    menu_recycle_layout.setVisibility(View.GONE);
+                    menu_recycle_layout2.setVisibility(View.VISIBLE);
+                    UserMenu menu_card = mData.get(pos);
+                    menu_recycle_name2.setText(menu_card.getUser_menu1());
+                }
                 }
             });
 
         }
 
-        public void setItem(main_content_card item) {
+        public void setItem(UserMenu item) {
 
-            title.setText(item.getTitle());
-            content.setText(item.getContent());
-            tag1.setText(item.getTag1());
-            tag2.setText(item.getTag2());
-            tag3.setText(item.getTag3());
-            url.setText(item.getUrl());
-            num_phase.setText(item.getNum_phase());
+            menu_recycle_name.setText(item.getUser_menu1());
         }
 
 
